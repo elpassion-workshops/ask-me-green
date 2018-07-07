692e06270df6677519e20d366714c222fa49120f
rmnlknv <snovix@gmail.com>
2018-07-07 15:38:03 +0200

    Added test to check if user signed in before let him create question

--- spec/features/new_question_spec.rb
+++ spec/features/new_question_spec.rb
@@ -17,6 +17,11 @@ RSpec.describe 'Logged user creates new question' do
     click_button 'Create Question'
 
     expect(page).to have_content 'Question successfully created'
+  end
 
+  scenario 'Unsigned user can\'t ask a question' do
+    visit new_question_path
+
+    expect(page).to have_content 'You need to sign in or sign up before continuing.'
   end
 end
