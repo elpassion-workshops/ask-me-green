254e3275de0c35941f6c46684eb44d7e26f60f88
rmnlknv <snovix@gmail.com>
2018-07-07 15:59:32 +0200

    Fixed spaces in questions_controller

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -6,23 +6,23 @@ class QuestionsController < ApplicationController
   end
 
   def new
-  	@question = Question.new
+    @question = Question.new
   end
 
   def create
-  	@question = current_user.questions.new(question_params)
-  	if @question.save
-  		flash[:notice] = "Question successfully created"
-  		redirect_to questions_path
-  	else
-  		flash[:alert] = "Error"
-  		render :new
-  	end
+    @question = current_user.questions.new(question_params)
+    if @question.save
+      flash[:notice] = "Question successfully created"
+      redirect_to questions_path
+    else
+      flash[:alert] = "Error"
+      render :new
+    end
   end
 
   private 
   def question_params
-	params.require(:question).permit(:title, :content)
+    params.require(:question).permit(:title, :content)
   end
 
 end
